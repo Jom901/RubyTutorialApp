11b1d31412b397b5bcd4f1f0637a4a0e50006cf8
Jonathan Medina <jom901@gmail.com>
2015-07-26 00:06:12 -0400

    Finished static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,15 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Jom Snow Site"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Jom Snow Site"
   end
 
   test "should get about" do
   	get :about
   	assert_response :success
+  	assert_select "title", "About | Jom Snow Site"
   end
 end
